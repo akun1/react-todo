e0d7e768addba85612ba66882617718be87a8b36
Akash Kundu <akashkundu92@gmail.com>
2019-05-10 13:53:29 -0500

    todos + state setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    todos: [
+      {
+        title: 'take out trash',
+        id: '1',
+        completed: false
+      },
+      {
+        title: 'cook dinner',
+        id: '2',
+        completed: false
+      },
+      {
+        title: 'mow lawn',
+        id: '3',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
