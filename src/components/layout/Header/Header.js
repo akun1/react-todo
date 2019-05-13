3b3e7abd996e45491b7f121569fe2932f785e2b8
Akash Kundu <akashkundu92@gmail.com>
2019-05-13 16:24:12 -0500

    added links to header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header id='header-container'>
       <h1>Todo List</h1>
+      <Link to='/' className='header-link'>Home</Link> | <Link to='/about' className='header-link'>About</Link>
     </header>
   )
 }
