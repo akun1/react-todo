2ac0218f87809903ccb4eb1bab67195f5804fd0c
Akash Kundu <akashkundu92@gmail.com>
2019-05-14 00:24:21 -0500

    hooked up redux+react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 
 //create initial state object
 const initialState = {
@@ -78,19 +76,21 @@ store.subscribe(() => {
 });
 //perform a function on the store with dispatch.
 //Requires a type and payload
-store.dispatch({
-    type: "ADD",
-    payload: 10
-});
-store.dispatch({
-    type: "SUBTRACT",
-    payload: 5
-});
-store.dispatch({
-    type: "SET_NAME",
-    payload: "summer"
-});
-store.dispatch({
-    type: "SET_AGE",
-    payload: 69
-});
+// store.dispatch({
+//     type: "ADD",
+//     payload: 10
+// });
+// store.dispatch({
+//     type: "SUBTRACT",
+//     payload: 5
+// });
+// store.dispatch({
+//     type: "SET_NAME",
+//     payload: "summer"
+// });
+// store.dispatch({
+//     type: "SET_AGE",
+//     payload: 69
+// });
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
