49059ffa9ca517d5fffe933362a280a087bea03d
Akash Kundu <akashkundu92@gmail.com>
2019-05-13 19:28:46 -0500

    added all css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
 
 function About() {
   return (
-    <React.Fragment>
-        <h1>About</h1>
-        <p>This is the Todo list app. V1.0.0.</p>
-    </React.Fragment>
+    <div id='about-container'>
+        <h2>About</h2>
+        <p>This is the Todo list app. V1.0.0. Thanks to <a href="https://www.youtube.com/watch?v=sBws8MSXN7A&list=LLT91ZPGSB3z1gHG4lxast_A&index=6&t=1479s" id='tutorial-link'>this tutorial</a> </p>
+    </div>
   )
 }
 
